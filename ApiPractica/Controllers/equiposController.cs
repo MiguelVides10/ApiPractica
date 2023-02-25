22c9b5e462f1d5353ecd66a1b7efe54fdd269646
Miguel Vides <miguelderas2025@gmail.com>
2023-02-25 10:04:46 -0600

    Actualizar, mostrar por id, mostrar con nombre, eliminar. Metodos agregados.

--- ApiPractica/Controllers/equiposController.cs
+++ ApiPractica/Controllers/equiposController.cs
@@ -1,6 +1,8 @@
 ﻿using ApiPractica.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiPractica.Controllers
 {
@@ -9,9 +11,9 @@ namespace ApiPractica.Controllers
     public class equiposController : ControllerBase
     {
         private readonly equiposContext _equiposContext;
-        
+
         public equiposController(equiposContext equipoContexto) {
-            _equiposContext= equipoContexto;
+            _equiposContext = equipoContexto;
         }
 
         [HttpGet]
@@ -19,12 +21,102 @@ namespace ApiPractica.Controllers
 
         public IActionResult Get()
         {
-            List<equipos> listadoEquipos  = (from e in _equiposContext.Equipos select e).ToList();
-            if(listadoEquipos.Count == 0)
+            List<equipos> listadoEquipos = (from e in _equiposContext.Equipos select e).ToList();
+            if (listadoEquipos.Count == 0)
             {
                 return NotFound();
             }
             return Ok(listadoEquipos);
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.Equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        [HttpGet]
+        [Route("find/{filtro}")]
+
+        public IActionResult FinfByDescription(string filtro) {
+            List<equipos> equipos = (from e in _equiposContext.Equipos
+                                     where e.descripcion.Contains(filtro)
+                                     select e).ToList();
+            if (equipos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipos);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarEquipo([FromBody] equipos equipos)
+        {
+            try
+            {
+                _equiposContext.Equipos.Add(equipos);
+                _equiposContext.SaveChanges();
+                return Ok();
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar) {
+            equipos? equipoActual = (from e in _equiposContext.Equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipoActual == null)
+            {
+                return NotFound();
+            }
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContext.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContext.SaveChanges();
+
+            return Ok(equipoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.Equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            _equiposContext.Attach(equipo);
+            _equiposContext.Remove(equipo);
+            _equiposContext.SaveChanges(); 
+            return Ok(equipo);
         }
     }
 }
--- ApiPractica/Program.cs
+++ ApiPractica/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<equiposContext>(options =>
     options.UseSqlServer(
             builder.Configuration.GetConnectionString("equiposDbConnection")
         ) );
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
